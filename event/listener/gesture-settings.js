1b6ee9558b208fcbbad72dfde2a4f18e86ed0ca1
ttg1109s <ttg1109s@gmail.com>
2026-08-11 03:23:53 -0700

    Add files via upload

--- event/listener/gesture-settings.js
+++ event/listener/gesture-settings.js
@@ -19,10 +19,10 @@ const GESTURE_SETTINGS_INPUT_MAP = {
     'setting-gesture-action-swipe-right': { type: 'gestureSettings.swipeRight.change', checkbox: false },
     'setting-gesture-action-tap-single': { type: 'gestureSettings.tapSingle.change', checkbox: false },
     'setting-gesture-action-tap-double': { type: 'gestureSettings.tapDouble.change', checkbox: false },
+    'setting-gesture-triple-tap-enable': { type: 'gestureSettings.tripleTapEnable.change', checkbox: true },
+    'setting-gesture-triple-tap-target': { type: 'gestureSettings.tripleTapTarget.change', checkbox: false },
     'setting-gesture-seek-hold-enable': { type: 'gestureSettings.seekHoldEnable.change', checkbox: true },
     'setting-gesture-edge-top': { type: 'gestureSettings.edgeTop.change', checkbox: true },
-    'setting-gesture-edge-bottom': { type: 'gestureSettings.edgeBottom.change', checkbox: true },
-    'setting-gesture-edge-bottom-target': { type: 'gestureSettings.edgeBottomTarget.change', checkbox: false },
 };
 
 if (settingsStackBody) {
